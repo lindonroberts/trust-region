Bugfix: step calculation for linear models without box constraints

trustregion/interface.py
@@ -102,7 +102,7 @@ def solve(g, H, delta, sl=None, su=None, verbose_output=False):
         crvmin = 0.0 if delta <= ZERO_THRESH else -1.0
     elif linear and not box_constrained:
         # Minimise linear function inside a ball - analytic solution
-        s = g * (delta / np.linalg.norm(g))
+        s = -g * (delta / np.linalg.norm(g))
         crvmin = 0.0
     elif linear:
         # Linear with box constraints - TRSLIN

trustregion/tests/test_interface.py
@@ -375,7 +375,7 @@ class TestAnalytic(unittest.TestCase):
         delta = 5.0
         for H in [None, np.zeros((len(g), len(g)))]:
             x = trustregion.solve(g, H, delta)
-            xtrue = delta * g / np.linalg.norm(g)
+            xtrue = -delta * g / np.linalg.norm(g)
             self.assertTrue(np.max(np.abs(x - xtrue)) < 1e-10, msg='Wrong step')
             self.assertTrue(np.linalg.norm(x) <= delta + BALL_EPS, msg='Ball constraint violated')
 