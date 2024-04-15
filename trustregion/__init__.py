2738688ad101ba16fd0e66d17639e6d91100ccfd
Lindon Roberts <lindon.roberts@anu.edu.au>
2024-04-15 15:27:20 +1000

    Increment version number

--- trustregion/__init__.py
+++ trustregion/__init__.py
@@ -1,7 +1,7 @@
 # Ensure compatibility with Python 2
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = '1.2'
+__version__ = '1.2.1'
 
 from .interface import solve
 __all__ = ['solve']
